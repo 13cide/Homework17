f6dca1266f1fcb347bdcf177b5f383ac2606bfe4
Aleksey Khlopkov <92057605+13cide@users.noreply.github.com>
2023-01-23 11:10:41 +0300

    Update MySurfaceView.java

--- app/src/main/java/com/example/surfacehomework/MySurfaceView.java
+++ app/src/main/java/com/example/surfacehomework/MySurfaceView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.util.Log;
 import android.util.Pair;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
@@ -17,12 +16,12 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
     private SurfaceHolder surfaceholder;
     private final DrawThread drawThread;
 
-    private Canvas canvas;
+
     private Pair<Float, Float> circlePosition;
     private Pair<Float, Float> nextPosition;
 
-    private final long UPDATE_TIME = 20;
-    private final float STEP_SIZE = 20.0f;
+    private static final long UPDATE_TIME = 20;
+    private static final float STEP_SIZE = 20.0f;
 
 
     public MySurfaceView(Context context) {
@@ -37,6 +36,20 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
         drawThread.start();
     }
 
+    public void calculateNewPosition() {
+        float dx = nextPosition.first - circlePosition.first;
+        float dy = nextPosition.second - circlePosition.second;
+        float length = (float) Math.sqrt(dx * dx + dy * dy);
+
+        if (length <= STEP_SIZE) {
+            circlePosition = nextPosition;
+        } else {
+            float delta = length / STEP_SIZE;
+            dx /= delta;
+            dy /= delta;
+            circlePosition = new Pair<>(circlePosition.first + dx, circlePosition.second + dy);
+        }
+    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -46,23 +59,9 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
 
     class DrawThread extends Thread {
         private volatile boolean running = true;
+        private Canvas canvas;
 
 
-        public void calculateNewPosition() {
-            float dx = nextPosition.first - circlePosition.first;
-            float dy = nextPosition.second - circlePosition.second;
-            float length = (float) Math.sqrt((double) (dx*dx + dy*dy));
-
-            if (length <= STEP_SIZE){
-                circlePosition = nextPosition;
-            }
-            else {
-                float delta = length/STEP_SIZE;
-                dx /= delta;
-                dy /= delta;
-                circlePosition = new Pair<>(circlePosition.first + dx, circlePosition.second + dy);
-            }
-        }
 
         @Override
         public void run() {
@@ -70,21 +69,23 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
             paint.setColor(Color.RED);
             nextPosition = circlePosition = new Pair<>(getWidth() / 2.0f, getHeight() / 2.0f);
 
-            try {
-                while (running) {
+
+            while (running) {
+                try {
                     Thread.sleep(UPDATE_TIME);
                     calculateNewPosition();
                     canvas = surfaceholder.lockCanvas();
                     canvas.drawColor(Color.CYAN);
                     canvas.drawCircle(circlePosition.first, circlePosition.second, 50, paint);
+
+                } catch (Exception ignored) {
+                } finally {
                     surfaceholder.unlockCanvasAndPost(canvas);
                 }
-            } catch (Exception ignored) {}
-            finally {
-                
             }
         }
     }
+
 
     @Override
     public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
